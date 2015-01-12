cf9d8e99ded26ce7a67a671e2a74af1a81ca7b67
Luiz Sanches <luizgrsanches@gmail.com>
2015-01-12 18:50:35 -0300

    obter a linha correta do processo do servidor rails de teste

--- spec_acceptance.sh
+++ spec_acceptance.sh
@@ -6,4 +6,4 @@ RAILS_ENV=test rails s -p 3002 -d
 
 cd ../gsan_online
 rspec spec_acceptance/*
-ps aux | grep "ruby bin/rails s -p 3002" | head -1 | awk '{print $2}' | xargs kill
+ps aux | grep "ruby bin/rails s -p 3002" | sed "/0:00/d" | awk '{print $2}' | xargs kill
