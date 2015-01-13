e721ebdc46aa1d20cc67c2690ea7c722afbac533
Felipe Iketani <felipeiketani@gmail.com>
2015-01-13 16:19:36 -0300

    WIP Pesquisar CEPs

--- app/assets/javascripts/gsan/cep/ceps_index_controller.js
+++ app/assets/javascripts/gsan/cep/ceps_index_controller.js
@@ -2,7 +2,13 @@ var app = angular.module("gsan");
 
 app.controller("CepsIndexController", ["Flash", "$scope", "$http", "CadastroUrl", function(Flash, $scope, $http, CadastroUrl) {
   $scope.flash = Flash;
-  $http.get(CadastroUrl() + "/ceps").success(function(data) {
-    $scope.ceps = data;
-  });
+
+  $scope.query = { cep_cdcep: "68508060" };
+
+  $scope.pesquisar = function() {
+    var query = $.param({ query: $scope.query });
+    $http.get(CadastroUrl() + "/ceps?" + query).success(function(data) {
+      $scope.ceps = data;
+    });
+  }
 }]);
