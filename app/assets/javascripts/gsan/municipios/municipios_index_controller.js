f74a53dab6f5e3e1508aaffed4598b5c4436f5b1
Paulo Moura <paulociecomp@gmail.com>
2015-01-14 12:03:00 -0300

    paginacao de municipios

--- app/assets/javascripts/gsan/municipios/municipios_index_controller.js
+++ app/assets/javascripts/gsan/municipios/municipios_index_controller.js
@@ -16,10 +16,41 @@ app.controller("MunicipiosIndexController", ["Flash", "$scope", "$http", "Cadast
     $scope.regioes_desenvolvimento = data;
   });
 
+  $scope.queryVazia = function() {
+    for (var input in $scope.query) {
+      if ($scope.query[input] !== "" && $scope.query[input] !== undefined) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  $scope.pagina = function(soma) {
+    if ($scope.page.current_page + soma > $scope.page.total_pages ||
+        $scope.page.current_page + soma < 1) {
+      return
+    }
+    $scope.queryCache.page = $scope.page.current_page + soma;
+
+    submeterPesquisa();
+  }
+
   $scope.pesquisar = function() {
-    var query = $.param({ query: $scope.query });
-    $http.get(CadastroUrl() + "/municipios?" + query).success(function(data) {
-      $scope.municipios = data;
+    var copiedQuery = jQuery.extend({},$scope.query);
+    $scope.queryCache = { query: copiedQuery };
+    submeterPesquisa();
+  }
+
+  var submeterPesquisa = function() {
+    var query = $.param($scope.queryCache);
+    $scope.loading = true;
+    $http.get(CadastroUrl() + "/municipios?" + query)
+    .success(function(data) {
+      $scope.municipios = data.municipios;
+      $scope.page = data.page
+      $scope.loading = false;
+    }).error(function() {
+      $scope.loading = false;
     });
   }
 }]);
