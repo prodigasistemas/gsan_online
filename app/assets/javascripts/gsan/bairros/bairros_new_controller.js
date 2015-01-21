f34bd975b9e58d57b9276f5e1df88a0e87c25a0d
Luiz Sanches <luizgrsanches@gmail.com>
2015-01-21 16:14:54 -0300

    cadastrar areas do bairro

--- app/assets/javascripts/gsan/bairros/bairros_new_controller.js
+++ app/assets/javascripts/gsan/bairros/bairros_new_controller.js
@@ -2,9 +2,8 @@ var app = angular.module("gsan");
 
 app.controller("BairrosNewController", ["Bairro", "Municipio", "DistritoOperacional", "CadastroUrl", "$scope", "$http", "$location", "Flash", function(Bairro, Municipio, DistritoOperacional, CadastroUrl, $scope, $http, $location, Flash) {
   $scope.bairro = {ativo: 1};
-  //$scope.bairro_area = {};
-  //$scope.distrito_operacional = {};
-  //$scope.bairro.areas = [];
+  $scope.bairro_area = {};
+  $scope.distrito_operacional = {};
 
   $scope.municipios = Municipio.query();
   $scope.distrito_operacionais = DistritoOperacional.query();
@@ -12,18 +11,38 @@ app.controller("BairrosNewController", ["Bairro", "Municipio", "DistritoOperacio
   $scope.atualizaMunicipioId = function() {
     $scope.bairro.municipio_id = $scope.bairro.municipio.id;
   };
-/*
+
   $scope.adicionaArea = function() {
-    if (areaSelecionada()) { return; }
-    $scope.bairro.bairro_areas.push({nome: $scope.bairro_area.nome, distrito_operacional: $scope.distrito_operacional.selecionado});
+    //if (areaSelecionada()) { return; }
+    $scope.bairro.bairro_areas = $scope.bairro.bairro_areas || [];
+
+    $scope.bairro.bairro_areas.push({
+      nome: $scope.bairro_area.nome,
+      distrito_operacional_id: $scope.distrito_operacional.selecionado.id,
+      distrito_operacional_descricao: $scope.distrito_operacional.selecionado.descricao
+    });
+
+    $scope.bairro_area.nome = "";
+    angular.element('#bairro_area_nome').focus();
   };
 
   $scope.removeArea = function(bairroArea) {
     var index = $scope.bairro.bairro_areas.indexOf(bairroArea);
     $scope.bairro.bairro_areas.splice(index, 1);
   };
-*/
+
+  var construirParametrosParaBairroAreas = function() {
+    if (!$scope.bairro.bairro_areas) { return; }
+    $scope.bairro.bairro_areas_attributes = {};
+
+    $.each($scope.bairro.bairro_areas, function(index, bairro_area) {
+      $scope.bairro.bairro_areas_attributes[index] = bairro_area
+    });
+  };
+
   $scope.submeter = function() {
+    construirParametrosParaBairroAreas();
+
     var bairro = new Bairro({bairro: $scope.bairro});
     bairro.$save(function() {
       Flash.setMessage("Bairro criado com sucesso");
