5b42c0b4e2fc8421ad8a3bf2f29ccf992674a3af
Paulo Igor <piagodinho@gmail.com>
2015-01-22 16:12:07 -0300

    melhorias no footer e menu

--- app/assets/javascripts/gsan/controllers/nav_controller.js
+++ app/assets/javascripts/gsan/controllers/nav_controller.js
@@ -1,12 +1,22 @@
 var app = angular.module("gsan");
 
 app.controller("NavController", ["$rootScope", "$scope", "$location", function($rootScope, $scope, $location) {
+  $scope.states = {};
+  $scope.states.activeItem = 'ceps';
+  $scope.items = [
+    { id: 'ceps',         title: 'CEPs' },
+    { id: 'municipios',   title: 'Municípios' },
+    { id: 'bairros',      title: 'Bairros' },
+    { id: 'logradouros',  title: 'Logradouros' }
+  ];
+
   $rootScope.$on("$routeChangeSuccess", function() {
     $scope.changeTab();
   });
 
   $scope.changeTab = function() {
     $scope.currentPage = $location.url();
+    $scope.states.activeItem = $location.url().substring(1, $location.url().length);
     $scope.ceps = $location.url().match(/ceps/) ? true : false;
     $scope.municipios = $location.url().match(/municipios/) ? true : false;
   }
