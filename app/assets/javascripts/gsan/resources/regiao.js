0a3b295267f2c3d778716c7baf455a7a4b4336b3
Paulo Moura <paulociecomp@gmail.com>
2015-01-23 12:01:25 -0300

    correcao a query no resource regiao

--- app/assets/javascripts/gsan/resources/regiao.js
+++ app/assets/javascripts/gsan/resources/regiao.js
@@ -3,6 +3,11 @@ var app = angular.module("gsan");
 app.factory("Regiao", ["$resource", "CadastroUrl", function($resource, CadastroUrl) {
   var regiao = $resource(CadastroUrl() + "/regioes/:id", { id: "@id" },
     {
+      'query': {
+        method: 'GET',
+        transformResponse: function (data) {return angular.fromJson(data).regioes},
+        isArray: true
+      },
       'update': { method:'PUT', isArray: false }
     }
   );
