853304348ba6c5ae3d59059eff1d81d9ce03c7b1
Paulo Moura <paulociecomp@gmail.com>
2015-01-27 11:57:31 -0300

    loader de ajax para qualquer requisicao

---
+++ app/assets/javascripts/gsan/directives/loader.js
@@ -0,0 +1,13 @@
+var app = angular.module("gsan");
+
+app.directive("loader", function ($rootScope) {
+    return function ($scope, element, attrs) {
+        $scope.$on("loader_show", function () {
+            return element.show();
+        });
+        return $scope.$on("loader_hide", function () {
+            return element.hide();
+        });
+    };
+}
+)
---
+++ app/assets/javascripts/gsan/factories/htp_interceptor.js
@@ -0,0 +1,40 @@
+var app = angular.module("gsan");
+
+app.factory('httpInterceptor', function ($q, $rootScope, $log) {
+
+    var numLoadings = 0;
+
+    return {
+        request: function (config) {
+
+            numLoadings++;
+
+            // Show loader
+            $rootScope.$broadcast("loader_show");
+            return config || $q.when(config)
+
+        },
+        response: function (response) {
+
+            if ((--numLoadings) === 0) {
+                // Hide loader
+                $rootScope.$broadcast("loader_hide");
+            }
+
+            return response || $q.when(response);
+
+        },
+        responseError: function (response) {
+
+            if (!(--numLoadings)) {
+                // Hide loader
+                $rootScope.$broadcast("loader_hide");
+            }
+
+            return $q.reject(response);
+        }
+    };
+})
+.config(function ($httpProvider) {
+    $httpProvider.interceptors.push('httpInterceptor');
+});
