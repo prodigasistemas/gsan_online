fdf4b1ede487526b1fd7d1254819671f3c678b1f
Felipe Iketani <felipeiketani@gmail.com>
2015-01-28 16:54:37 -0300

    Escolheando melhor os icones

--- app/assets/javascripts/gsan/controllers/painel_controller.js
+++ app/assets/javascripts/gsan/controllers/painel_controller.js
@@ -16,27 +16,27 @@ app.controller("PainelController", ["$route", "$scope", "$location", function($r
       items: [
         { grupo: "enderecos", icone: 'fa fa-envelope',        url: '/ceps',                     titulo: 'CEPs' },
         { grupo: "enderecos", icone: 'fa fa fa-building',     url: '/municipios',               titulo: 'Municípios' },
-        { grupo: "enderecos", icone: 'fa fa-location-arrow',  url: '/bairros',                  titulo: 'Bairros' },
-        { grupo: "enderecos", icone: 'fa fa-map-marker',      url: '/logradouros',              titulo: 'Logradouros' },
+        { grupo: "enderecos", icone: 'fa fa-flag',            url: '/bairros',                  titulo: 'Bairros' },
+        { grupo: "enderecos", icone: 'fa fa-location-arrow',  url: '/logradouros',              titulo: 'Logradouros' },
         { grupo: "enderecos", icone: 'fa fa-tree',            url: '/micro_regioes',            titulo: 'Micro Regiões' },
-        { grupo: "enderecos", icone: 'fa fa-sun-o',           url: '/regioes',                  titulo: 'Regiões' },
+        { grupo: "enderecos", icone: 'fa fa-photo',           url: '/regioes',                  titulo: 'Regiões' },
         { grupo: "enderecos", icone: 'fa fa-sun-o',           url: '/regioes_desenvolvimento',  titulo: 'Regiões de Desenvolvimento' },
         { grupo: "enderecos", icone: 'fa fa-university',      url: '',                          titulo: 'Unidade de Federação' },
         { grupo: "enderecos", icone: 'fa fa-envelope-square', url: '/cep_tipos',                titulo: 'Tipos de CEP' },
-        { grupo: "enderecos", icone: 'fa fa-cube',            url: '',                          titulo: 'Títulos de Logradouro' },
-        { grupo: "enderecos", icone: 'fa fa-asterisk',        url: '',                          titulo: 'Tipos de Logradouro' }
+        { grupo: "enderecos", icone: 'fa fa-text-height',     url: '',                          titulo: 'Títulos de Logradouro' },
+        { grupo: "enderecos", icone: 'fa fa-cubes',           url: '',                          titulo: 'Tipos de Logradouro' }
       ]
     },{
       titulo: "Clientes",
       items: [
-        { grupo: "clientes", icone: 'fa fa-users',        url: '/clientes',        titulo: 'Clientes' },
-        { grupo: "clientes", icone: 'fa fa-map-marker',   url: '/cliente_tipos',   titulo: 'Tipos de Clientes' },
-        { grupo: "clientes", icone: 'fa fa-circle',       url: '/esferas_poder',   titulo: 'Esferas de Poder' },
-        { grupo: "clientes", icone: 'fa fa-street-view',  url: '',                 titulo: 'Endereço de Referência' },
-        { grupo: "clientes", icone: 'fa fa-circle',       url: '/ramos_atividade', titulo: 'Ramos de Atividade' },
-        { grupo: "clientes", icone: 'fa fa-university',   url: '',                 titulo: 'Órgão Expedidor de RG' },
-        { grupo: "clientes", icone: 'fa fa-gavel',        url: '/profissoes',      titulo: 'Profissões' },
-        { grupo: "clientes", icone: 'fa fa-archive',      url: '/endereco_tipos',  titulo: 'Tipos de Endereço' }
+        { grupo: "clientes", icone: 'fa fa-users',          url: '/clientes',        titulo: 'Clientes' },
+        { grupo: "clientes", icone: 'fa fa-cubes',          url: '/cliente_tipos',   titulo: 'Tipos de Clientes' },
+        { grupo: "clientes", icone: 'fa fa-circle',         url: '/esferas_poder',   titulo: 'Esferas de Poder' },
+        { grupo: "clientes", icone: 'fa fa-map-marker' ,    url: '',                 titulo: 'Endereço de Referência' },
+        { grupo: "clientes", icone: 'fa fa-gavel',          url: '/ramos_atividade', titulo: 'Ramos de Atividade' },
+        { grupo: "clientes", icone: 'fa fa-university',     url: '',                 titulo: 'Órgão Expedidor de RG' },
+        { grupo: "clientes", icone: 'fa fa-graduation-cap', url: '/profissoes',      titulo: 'Profissões' },
+        { grupo: "clientes", icone: 'fa fa-home',           url: '/endereco_tipos',  titulo: 'Tipos de Endereço' }
       ]
     }
   ];
