a297bfc9d33ea811b22238a57e73255ac2f502e5
Paulo Moura <paulociecomp@gmail.com>
2015-01-27 22:02:36 -0300

    correcao de consulta no cep tipo

--- app/assets/javascripts/gsan/resources/cep_tipo.js
+++ app/assets/javascripts/gsan/resources/cep_tipo.js
@@ -3,6 +3,11 @@ var app = angular.module("gsan");
 app.factory("CepTipo", ["$resource", "CadastroUrl", function($resource, CadastroUrl) {
   var cepTipo = $resource(CadastroUrl() + "/cep_tipos/:id", { id: "@id" },
     {
+      'query': {
+          method: 'GET',
+          transformResponse: function (data) {return angular.fromJson(data).cep_tipos},
+          isArray: true
+      },
       'update': { method:'PUT', isArray: false }
     });
 
