5209809cdb9919981551fd7509d3797216af2cdb
Paulo Moura <paulociecomp@gmail.com>
2015-01-29 11:48:19 -0300

    correcao de componente unidade federacao

--- app/assets/javascripts/gsan/resources/unidade_federacao.js
+++ app/assets/javascripts/gsan/resources/unidade_federacao.js
@@ -3,6 +3,11 @@ var app = angular.module("gsan");
 app.factory("UnidadeFederacao", ["$resource", "CadastroUrl", function($resource, CadastroUrl) {
   var uf = $resource(CadastroUrl() + "/unidade_federacoes/:ufId", { ufId: "@id" },
     {
+      'query': {
+          method: 'GET',
+          transformResponse: function (data) {return angular.fromJson(data).unidade_federacoes},
+          isArray: true
+      },
       'update': { method:'PUT', isArray: false }
     });
 
